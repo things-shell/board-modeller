cfec27931720ddccd0b5d2376a73c34750d17a78
Hearty Oh <heartyoh@hatiolab.com>
2019-02-20 10:38:34 +0900

    v0.2.1
    things-component module의 editor 를 위한 클래스 export

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 export * from './board-modeller'
 export * from './edit-toolbar'
+
+/* 
+ ThingsEditorProperty는 
+ Things Scene 컴포넌트에서 속성에디터 개발 시에 참조하기 위해서 export 함.
+ */
+export { default as ThingsEditorProperty } from './editors/things-editor-property'
